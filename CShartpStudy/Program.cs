0de86b041b407570fe1585ff6cb794c9fe6e496b
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-18 06:14:16 +0900

    [김동찬] C Sharp 공부

--- CShartpStudy/Program.cs
+++ CShartpStudy/Program.cs
@@ -2,91 +2,16 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            //카멜
-            while (true)
+            for (int i = 0; i < 100; ++i)
             {
-                Random rand = new Random();
-                int aiChoice = rand.Next(0, 3);
-
-                int Choice = Convert.ToInt32(Console.ReadLine());
-
-                switch (Choice)
+                if (i % 3 != 0)
                 {
-                    case 0:
-                        Console.WriteLine("당신은 주먹을 냈습니다");
-                        break;
-                    case 1:
-                        Console.WriteLine("당신은 가위을 냈습니다");
-                        break;
-                    case 2:
-                        Console.WriteLine("당신은 보을 냈습니다");
-                        break;
+                    continue;
                 }
-
-                switch (aiChoice)
-                {
-                    case 0:
-                        Console.WriteLine("컴퓨터는 주먹을 냈습니다");
-                        break;
-                    case 1:
-                        Console.WriteLine("컴퓨터는 가위을 냈습니다");
-                        break;
-                    case 2:
-                        Console.WriteLine("컴퓨터는 보을 냈습니다");
-                        break;
-                }
-
-
-                switch (Choice)
-                {
-                    case 0:
-
-                        switch (aiChoice)
-                        {
-                            case 0:
-                                Console.WriteLine("비겼습니다.");
-                                break;
-                            case 1:
-                                Console.WriteLine("이겼습니다");
-                                break;
-                            case 2:
-                                Console.WriteLine("졌습니다");
-                                break;
-                        }
-                        break;
-                    case 1:
-
-                        switch (aiChoice)
-                        {
-                            case 0:
-                                Console.WriteLine("졌습니다.");
-                                break;
-                            case 1:
-                                Console.WriteLine("비겼습니다");
-                                break;
-                            case 2:
-                                Console.WriteLine("이겼습니다");
-                                break;
-                        }
-                        break;
-                    case 2:
-
-                        switch (aiChoice)
-                        {
-                            case 0:
-                                Console.WriteLine("이겼습니다.");
-                                break;
-                            case 1:
-                                Console.WriteLine("졌습니다");
-                                break;
-                            case 2:
-                                Console.WriteLine("비겼습니다");
-                                break;
-                        }
-                        break;
-                }
+                Console.WriteLine($"무엇이냐면 {i}"); ;
             }
         }
     }
