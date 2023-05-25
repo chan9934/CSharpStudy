b04f62dc8b80d5bfe78af27beaf39e93955d2e8c
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-26 07:10:28 +0900

    [김동찬] CSharp 공부

--- CShartpStudy/Program.cs
+++ CShartpStudy/Program.cs
@@ -8,73 +8,20 @@ namespace CShartpStudy
 {
     internal class Program
     {
-        static int getHighScore(int[] score)
+        
+        class Knight
         {
-            int highScore = 0;
-            foreach(int a in score)
+
+            public int HP
             {
-                if(highScore < a)
-                {
-                    highScore = a;
-                }
-            }
-            return highScore;
+                get;
+                set;
+            } = 200;
         }
-        static int getAverageScore(int[] score)
-        {
-            int sum = 0;
-            for(int i = 0; i < score.Length; ++i)
-            {
-                sum += score[i];
-            }
-
-            return sum/score.Length;
-        }
-        static int genIndex(int[] score, int value)
-        {
-           for(int i = 0; i < score.Length; ++i)
-            {
-                if (score[i] == value)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-        static void sort(int[] score)
-        {
-            for(int i = 0; i < score.Length; ++i)
-            {
-                for(int j =  i; j < score.Length; ++j)
-                {
-                    if(j > i)
-                    {
-                       if( score[j] < score[j - 1])
-                        {
-                            int temp = score[j - 1];
-                            score[j - 1] = score[j];
-                            score[j] = temp;
-                        }
-                    }
-                }
-            }
-
-        }
-
-         
-
         static void Main(string[] args)
         {
-            int[] arr = new int[5] { 10, 30, 40, 20, 50 };
-            Console.WriteLine(getHighScore(arr)); 
-            Console.WriteLine(getAverageScore(arr)); 
-            Console.WriteLine(genIndex(arr, 30));
-            sort(arr);
-            foreach(int a in arr)
-            {
-                Console.WriteLine(a);
-            }
+            Knight knight = new Knight();
+            Console.WriteLine(knight.HP);
         }
     }
 }
