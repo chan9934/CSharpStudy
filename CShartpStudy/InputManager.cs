929a5e5a82b58253d8ac6c663009d281664b99ae
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-06-01 06:42:24 +0900

    [김동찬] C#마무리

--- CShartpStudy/InputManager.cs
+++ CShartpStudy/InputManager.cs
@@ -8,21 +8,21 @@ namespace CShartpStudy
 {
     internal class InputManager
     {
-        public delegate void OninputKey();
-        public event OninputKey InputKey;
-
+        public delegate void Click();
+        public event Click click;
 
         public void Update()
         {
             if (Console.KeyAvailable == false)
-            {
                 return;
-            }
+
             ConsoleKeyInfo info = Console.ReadKey();
-            if (info.Key == ConsoleKey.A)
+
+            if(info.Key == ConsoleKey.A)
             {
-                InputKey();
+                click();
             }
+            
         }
     }
 }
--- CShartpStudy/Program.cs
+++ CShartpStudy/Program.cs
@@ -3,23 +3,32 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
 
 namespace CShartpStudy
 {
+    class Mon
+    {
+        public int id { get; set; }
+    }
+
     internal class Program
     {
-        static public void Test2()
-        {
-            Console.WriteLine("a");
-        }
         static void Main(string[] args)
         {
-            InputManager inputmanager = new InputManager();
-            inputmanager.InputKey += Test2;
-            while(true)
+            int? a = null;
+            a = 3;
+            if(a.HasValue)
             {
-                inputmanager.Update();
+                int number = a.Value;
+                Console.WriteLine(number);
             }
+            int b = a ?? 0;
+
+            Mon mon = new Mon();
+            int? c = mon?.id;
+
         }
     }
 }
