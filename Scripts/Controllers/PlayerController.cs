68baf22ea9135251a3f441f72f758ecbe61a9891
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-07-26 08:02:07 +0900

    fedwsa

---
+++ Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+	PlayerStat _stat;
+
+	Vector3 _destPos;
+
+	Texture2D _attackIcon;
+	Texture2D _handIcon;
+
+	CursorType _cursorType = CursorType.None;
+
+    void Start()
+    {
+		_attackIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Attack");
+        _handIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Hand");
+		_stat = gameObject.GetComponent<PlayerStat>();	
+		Managers.Input.MouseAction -= OnMouseClicked;
+		Managers.Input.MouseAction += OnMouseClicked;		
+	}
+
+	public enum CursorType
+	{
+		None,
+		Attack,
+		Hand,
+	}
+	public enum PlayerState
+	{
+		Die,
+		Moving,
+		Idle,
+		Skill
+	}
+
+	PlayerState _state = PlayerState.Idle;
+
+	void UpdateDie()
+	{
+		// 아무것도 못함
+
+	}
+
+	void UpdateMoving()
+	{
+		Vector3 dir = _destPos - transform.position;
+		if (dir.magnitude < 0.1f)
+		{
+			_state = PlayerState.Idle;
+		}
+		else
+		{
+			float moveDist = Mathf.Clamp(_stat.MoveSpeed * Time.deltaTime, 0, dir.magnitude);
+			NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+			
+			nma.Move(dir.normalized * moveDist);
+			Debug.DrawRay(transform.position, dir.normalized, Color.green);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+            if (Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+			{
+				_state = PlayerState.Idle;
+				return;
+			}
+			//transform.position += dir.normalized * moveDist;
+		}
+
+		// 애니메이션
+		Animator anim = GetComponent<Animator>();
+		// 현재 게임 상태에 대한 정보를 넘겨준다
+		anim.SetFloat("speed", _stat.MoveSpeed);
+	}
+
+	void UpdateIdle()
+	{
+		// 애니메이션
+		Animator anim = GetComponent<Animator>();
+
+		anim.SetFloat("speed", 0);
+	}
+
+	void Update()
+    {
+		UpdateMouseCursor();
+
+        switch (_state)
+		{
+			case PlayerState.Die:
+				UpdateDie();
+				break;
+			case PlayerState.Moving:
+				UpdateMoving();
+				break;
+			case PlayerState.Idle:
+				UpdateIdle();
+				break;
+		}
+	}
+
+    int _mask = (1 << (int)Define.Layer.Monster) | (1 << (int)Define.Layer.Ground);
+    void UpdateMouseCursor()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 100.0f, _mask))
+		{
+			if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+			{
+				if (_cursorType != CursorType.Attack)
+				{
+					Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width / 5, 0), CursorMode.Auto);
+					_cursorType = CursorType.Attack;
+				}
+			}
+			else
+			{
+				if (_cursorType != CursorType.Hand)
+				{
+					Cursor.SetCursor(_handIcon, new Vector2(_handIcon.width / 3, 0), CursorMode.Auto);
+					_cursorType = CursorType.Hand;
+				}
+			}
+		}
+
+	}
+
+	void OnMouseClicked(Define.MouseEvent evt)
+	{
+		if (_state == PlayerState.Die)
+			return;
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100.0f, _mask))
+		{
+			_destPos = hit.point;
+			_state = PlayerState.Moving;
+			if(hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+			{
+				Debug.Log("Attack");
+			}
+			else
+			{
+				Debug.Log("Ground");
+			}
+		}
+	}
+}
